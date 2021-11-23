3338bc5bce41b75342142bc9f8bb87b198f5b7e3
YeJun <hayejun1013@naver.com>
2021-11-23 18:36:12 +0900

    feat: add model classes and migration files

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.CharField(max_length=32, unique=True, primary_key=True)
+    password = models.CharField(max_length=128)
+    cars = models.ManyToManyField("cars.Trim", through="CarOwner")
+
+    class Meta:
+        db_table = "users"
+
+
+class CarOwner(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    trim = models.ForeignKey("cars.Trim", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "cars_owners"
