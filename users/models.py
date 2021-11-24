69190b09da8990f7c71035851407b77930a037a5
YeJun <hayejun1013@naver.com>
2021-11-25 00:53:28 +0900

    fix : fix car app model schema

--- users/models.py
+++ users/models.py
@@ -4,15 +4,16 @@ from django.db import models
 class User(models.Model):
     id = models.CharField(max_length=32, unique=True, primary_key=True)
     password = models.CharField(max_length=128)
-    cars = models.ManyToManyField("cars.Trim", through="CarOwner")
+    tires = models.ManyToManyField("cars.Tire", through="CarOwner")
 
     class Meta:
         db_table = "users"
 
 
 class CarOwner(models.Model):
+    model_name = models.CharField(max_length=32)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    trim = models.ForeignKey("cars.Trim", on_delete=models.CASCADE)
+    tire = models.ForeignKey("cars.Tire", on_delete=models.CASCADE)
 
     class Meta:
         db_table = "cars_owners"
