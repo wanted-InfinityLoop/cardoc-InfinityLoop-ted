692b6de873b593ab26628570ecfe5d07f0d5ae37
YeJun <hayejun1013@naver.com>
2021-11-24 17:48:14 +0900

    feat : add user auth post methods and test code
    - 회원가입, 로그인 Post 함수 구현
    - 유닛 테스트 구현

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,128 @@
-from django.test import TestCase
+import json
+import bcrypt
+import jwt
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from users.models import User
+from my_settings import MY_SECRET_KEY, ALGORITHM
+
+
+class SignupViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        User.objects.create(id="테스트유저", password="abcd12!@")
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_post_signup_success(self):
+        data = {"id": "테스트유저2", "password": "abcd12!@"}
+
+        response = self.client.post(
+            "/users/signup", json.dumps(data), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "User 테스트유저2 has created!"})
+
+    def test_post_signup_registered_id(self):
+        data = {"id": "테스트유저", "password": "abcd12!@"}
+
+        response = self.client.post(
+            "/users/signup", json.dumps(data), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message": "REGISTERED_ID"})
+
+    def test_post_signup_invalid_id(self):
+        data = {"id": "", "password": "abcd12!@"}
+
+        response = self.client.post(
+            "/users/signup", json.dumps(data), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message": "INVALID_ID_FORMAT"})
+
+    def test_post_signup_invalid_password_format(self):
+        data = {"id": "테스트유저2", "password": "abcd12"}
+
+        response = self.client.post(
+            "/users/signup", json.dumps(data), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message": "INVALID_PASSWORD_FORMAT"})
+
+    def test_post_signup_key_error(self):
+        data = {"password": "abcd12"}
+
+        response = self.client.post(
+            "/users/signup", json.dumps(data), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message": "KEY_ERROR"})
+
+
+class SigninViewTest(TestCase):
+    def setUp(self):
+        self.cilent = Client()
+
+        user1 = User.objects.create(
+            id="테스트유저1",
+            password=bcrypt.hashpw("abcd12!@".encode("utf-8"), bcrypt.gensalt()).decode(
+                "utf-8"
+            ),
+        )
+
+        self.access_token1 = jwt.encode({"user_id": user1.id}, MY_SECRET_KEY, ALGORITHM)
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_post_signin_success(self):
+        data = {"id": "테스트유저1", "password": "abcd12!@"}
+
+        response = self.client.post(
+            "/users/signin", json.dumps(data), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            {"message": "User 테스트유저1 has signed in!", "token": self.access_token1},
+        )
+
+    def test_post_signin_invalid_auth_request_id(self):
+        data = {"id": "테스트유저2", "password": "abcd12!@"}
+
+        response = self.client.post(
+            "/users/signin", json.dumps(data), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message": "INVALID_AUTH_REQUEST"})
+
+    def test_post_signin_invalid_auth_request_password(self):
+        data = {"id": "테스트유저1", "password": "abcd12!"}
+
+        response = self.client.post(
+            "/users/signin", json.dumps(data), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message": "INVALID_AUTH_REQUEST"})
+
+    def test_post_signin_key_error(self):
+        data = {"password": "abcd12!"}
+
+        response = self.client.post(
+            "/users/signin", json.dumps(data), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message": "KEY_ERROR"})
--- users/views.py
+++ users/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+import json
+import bcrypt
+import jwt
 
-# Create your views here.
+from django.views import View
+from django.http import JsonResponse
+
+from users.models import User
+from core.validation import password_validator
+from my_settings import MY_SECRET_KEY, ALGORITHM
+
+
+class SignupView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            id = data["id"]
+            password = data["password"]
+
+            if User.objects.filter(id=id).exists():
+                return JsonResponse({"message": "REGISTERED_ID"}, status=400)
+
+            if not id:
+                return JsonResponse({"message": "INVALID_ID_FORMAT"}, status=400)
+
+            if not password_validator(password):
+                return JsonResponse({"message": "INVALID_PASSWORD_FORMAT"}, status=400)
+
+            User.objects.create(
+                id=id,
+                password=bcrypt.hashpw(
+                    password.encode("utf-8"), bcrypt.gensalt()
+                ).decode("utf-8"),
+            )
+
+            return JsonResponse({"message": f"User {id} has created!"}, status=200)
+
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
+
+class SigninView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            if not User.objects.filter(id=data["id"]).exists():
+                return JsonResponse({"message": "INVALID_AUTH_REQUEST"}, status=400)
+
+            user = User.objects.get(id=data["id"])
+
+            if not bcrypt.checkpw(
+                data["password"].encode("utf-8"), user.password.encode("utf-8")
+            ):
+                return JsonResponse({"message": "INVALID_AUTH_REQUEST"}, status=400)
+
+            access_token = jwt.encode({"user_id": user.id}, MY_SECRET_KEY, ALGORITHM)
+
+            return JsonResponse(
+                {"message": f"User {user.id} has signed in!", "token": access_token},
+                status=200,
+            )
+
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
